3ed69d475becd218fcb366b1d2057998164a9f92
yc1121 <wherechi0912@gmail.com>
2023-12-12 00:25:25 +0800

    feat: user main page

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,7 +1,11 @@
+import UserProfile from '@/components/UserProfile';
+
 const page = () => {
   return (
-    <main className="flex flex-col justify-center min-h-full bg-gray-100 sm:p-5 lg:p-10 space-y-10">
-      <section>我的基本資料...</section>
+    <main className="flex flex-col justify-center bg-gray-100">
+      <section>
+        <UserProfile></UserProfile>
+      </section>
       <section>我主持的活動...挑幾個列在這</section>
       <section>我追蹤的活動...挑幾個列在這</section>
       <section>我參加的活動...挑幾個列在這</section>
