c30ca5e9d7b414d062678be12eb21195c7162ea3
RulerChen <a20196780@gmail.com>
2024-05-24 11:42:14 +0800

    fix: image limit size

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -56,7 +56,7 @@ io.on('connection', (socket) => {
 app.set('trust proxy', 1);
 
 app.use(compression());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
